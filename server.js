2d115bb9ea36ee4276bac153d7e9836f5373c7cd
Uzinex <uzdilovferuz@gmail.com>
2025-08-28 20:32:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname)); // отдаём всё из корня
+// Раздаём все статические файлы из корня
+app.use(express.static(__dirname));
 
+// Главная страница
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html")); // ищем именно в корне
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Запуск
 app.listen(process.env.PORT || 3000, () => {
   console.log("✅ Server running on port " + (process.env.PORT || 3000));
 });
