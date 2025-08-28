93eaee86a83efff89aaf9fc5b0360137f81aee5d
Uzinex <uzdilovferuz@gmail.com>
2025-08-28 20:37:11 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 
 // Главная страница
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "amira.html"));
 });
 
 // Запуск
