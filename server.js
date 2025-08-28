ee3909be9ff5b4dee705a9ee1971a1b4a162bab9
Uzinex <uzdilovferuz@gmail.com>
2025-08-28 20:31:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public"))); // если index.html лежит в public/
+app.use(express.static(__dirname)); // отдаём всё из корня
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html")); // ищем именно в корне
 });
 
 app.listen(process.env.PORT || 3000, () => {
